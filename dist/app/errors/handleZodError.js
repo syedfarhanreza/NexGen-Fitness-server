ec1c1130161cf6896fecbce248b194f5566af7cf
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-08-20 21:47:46 +0600

    deploy

--- dist/app/errors/handleZodError.js
+++ dist/app/errors/handleZodError.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const handleZodError = (err) => {
     const errorSources = err.errors.map((error) => {
-        return { path: error.path.join('.'), message: error.message };
+        return { path: error.path.join("."), message: error.message };
     });
     const statusCode = 400;
     return {
